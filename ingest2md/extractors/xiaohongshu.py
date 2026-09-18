283d93d0bcba5c608804d9b80392c407f64b40d4
leecolin2023 <leecolin2023@163.com>
2026-09-18 18:04:20 +0800

    Fix shared user-agent import

--- ingest2md/extractors/xiaohongshu.py
+++ ingest2md/extractors/xiaohongshu.py
@@ -15,7 +15,7 @@ from bs4 import BeautifulSoup
 from ingest2md.browser import load_netscape_cookies, launch_chromium
 from ingest2md.config import Settings, load_settings
 from ingest2md.htmlutils import clean_fragment, meta_content
-from ingest2md.netutils import DEFAULTDEFAULT_USER_AGENT
+from ingest2md.netutils import DEFAULT_USER_AGENT
 from ingest2md.model import Document, sanitize_filename
 from ingest2md.urlutils import host_of
 
